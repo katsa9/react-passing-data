1bd72b049e23c85345b4287259821663bcf97acb
katsa9 <kathleen.brand2@gmail.com>
2019-05-21 13:42:19 +0200

    first exercise

--- src/App.js
+++ src/App.js
@@ -105,8 +105,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
+		<ul>
         <h2>Favorite Movies</h2>
+          
+           {profiles.map(profile => {
+             const userName = users[profile.userID].name;
+			 const movieName = movies[profile.favoriteMovieID].name;
+    	     
+			return(
+              <li key={profile.id}>
+				<p>{`${userName}'s favorite movie is ${movieName}` }</p>
+              </li>
+              );
+			})}
+		</ul>
       </div>
+
     );
   }
 }
